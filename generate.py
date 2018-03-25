4bc7564c9dbec76eb4995ed24a5ddfedff6e1822
jinlinyi <jinlinyi@umich.edu>
2018-03-25 01:26:45 +0000

    finish coding

--- generate.py
+++ generate.py
@@ -3,26 +3,34 @@ import torch
 import tqdm
 import os
 import numpy as np
-from scipy.misc import imsave
+from scipy.misc import imsave, imresize
 
 import data.eecs442_challenge.ref as ds
 import train as net
 
 def evaluate():
-    test = ds.get_test_set()
+    #test = ds.get_test_set()
+    test = range(10)
     func, config = net.init()
 
     for idx in test:
         img = ds.load_image(idx, False)
         img = img / 255
         img = img.astype(np.float32)
-
-        output = (-1, config, phase='inference', imgs=img)
+        img = img[None, :, :, :]
+        img = torch.FloatTensor(img)
+        output = func(-1, config, phase='inference', imgs=img)
         pred = torch.FloatTensor(output['preds'][0][:, -1])
-        pred = pred / torch.norm(pred, 2, 3)
+        pred = pred[0, :, :, :]
+        pred = pred.permute(1, 2, 0)
+        for i in range(3):
+            pred[:,:,i] = pred[:,:,i] / torch.norm(pred, 2, 2)
 
         pred = pred * 255
-        imsave('./save/pred_{}.png'.format(idx), pred.astype(np.uint8))
+        pred = pred.numpy()
+        pred = pred.astype(np.uint8)
+        pred = imresize(pred, (128, 128))
+        imsave('./save/pred_{}.png'.format(idx), pred)
 
 
 if __name__=='__main__':
